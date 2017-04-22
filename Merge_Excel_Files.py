f49d724e965ddff9d7b3ff97673d440ae1fbf2aa
Rosanne Lai <rosannelai@gmail.com>
2017-04-22 19:31:45 -0400

    Add files via upload

---
+++ Merge_Excel_Files.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import glob2
+from os.path import basename as os
+
+all_data = pd.DataFrame()
+
+for f in glob2.glob("*.xls*"):
+    '''
+    sheetname parameter can either be an int for the index of the sheet tab,
+    or string for the sheet name
+    '''
+    df = pd.read_excel(f, sheetname="CF", index=False, header=None, names=None, ignore_index=True)
+    df.index = [os(f)] * len(df)
+    all_data = all_data.append(df)
+
+all_data.to_csv("MergedFile.csv", encoding='utf8')
+
+print ("Completed! Number of Files Merged = " + str(all_data.index.nunique()))
+print (all_data.index.unique())
